3cd6dc4c9a3ef94d64460fce3266d38f4134c540
shyamtiwari02062001 <shyam02062001@gmail.com>
2022-09-19 05:17:25 +0530

    Added styles les to tab bar and header

--- App.js
+++ App.js
@@ -5,13 +5,47 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import ManageExpense from './screens/manageExpenses';
 import RecentExpense from './screens/recentExpenses';
 import AllExpense from './screens/allExpenses';
+import {GlobalStyles} from './constants/styles';
+import {Image} from 'react-native';
 const Stack = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
 const ExpensesOverview = () => {
   return (
-    <BottomTabs.Navigator>
-      <BottomTabs.Screen name="RecentExpenses" component={RecentExpense} />
-      <BottomTabs.Screen name="AllExpenses" component={AllExpense} />
+    <BottomTabs.Navigator
+      screenOptions={{
+        headerStyle: {backgroundColor: GlobalStyles.colors.primary500},
+        headerTintColor: '#fff',
+        tabBarStyle: {backgroundColor: GlobalStyles.colors.primary500},
+        tabBarActiveTintColor: GlobalStyles.colors.accent500,
+      }}>
+      <BottomTabs.Screen
+        name="RecentExpenses"
+        component={RecentExpense}
+        options={{
+          title: 'Rexent Expenses',
+          tabBarLabel: 'Recent',
+          tabBarIcon: ({color, size}) => (
+            <Image
+              source={require('./assets/hourglass.png')}
+              style={{tintColor: color, height: size, width: size}}
+            />
+          ),
+        }}
+      />
+      <BottomTabs.Screen
+        name="AllExpenses"
+        component={AllExpense}
+        options={{
+          title: 'Rexent Expenses',
+          tabBarLabel: 'Recent',
+          tabBarIcon: ({color, size}) => (
+            <Image
+              source={require('./assets/calendar.png')}
+              style={{tintColor: color, height: size, width: size}}
+            />
+          ),
+        }}
+      />
     </BottomTabs.Navigator>
   );
 };
@@ -19,7 +53,11 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="ExpenseOverview" component={ExpensesOverview} />
+        <Stack.Screen
+          name="ExpenseOverview"
+          component={ExpensesOverview}
+          options={{headerShown: false}}
+        />
         <Stack.Screen name="ManageExpense" component={ManageExpense} />
       </Stack.Navigator>
     </NavigationContainer>
